2adc5e74aa9674f08526335072b7a0fdc105b9fd
sham <86479073+shamchalase@users.noreply.github.com>
2024-01-06 23:43:43 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import "./App.css";
-import Sidebar from './components/Sidebar';
-import Twittes from './components/Twittes';
-import Trends from './components/Trends';
-
-function App(){
-  return <div className="twitter">
-    <Sidebar />
-    <Twittes />
-    <Trends />
-  </div>;
-
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './ChatFeed';
+import './App.css';
+const App = ()=> {
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='sam'
+			userSecret='123123'
+			projectID='1f826895-ae3b-41c1-bf7e-f62bfbb14d7e'
+            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps }/>}
+            />
+	);
 }
+
 export default App;
